dab8c49f26f10d142e1d39255de5b8379b190cbf
rabs23 <ruthrabs22@gmail.com>
2023-12-20 06:55:21 -0800

    100-singly_linked_list.py

---
+++ 0x06-python-classes/100-singly_linked_list.py
@@ -0,0 +1,82 @@
+#!/usr/bin/python3
+
+
+"""Define classes for a singly-linked list."""
+
+
+class Node:
+    """Represent a node in a singly-linked list."""
+
+    def __init__(self, data, next_node=None):
+        """ Initialize a new Node
+
+        Args:
+            data (int): The data of the new Node.
+            next_node (Node): The next node of the new Node.
+        """
+        self.data = data
+        self.next_node = next_node
+
+    @property
+    def data(self):
+        """ Get/set the data of the Node """
+        return self.__data
+
+    @data.setter
+    def data(self, value):
+        if type(value) is not int:
+            raise TypeError("data must be an integer")
+        else:
+            self.__data = value
+
+    @property
+    def next_node(self):
+        """ Get/set the next_node of the Node """
+        return self.__next_node
+
+    @next_node.setter
+    def next_node(self, value):
+        if value is not None and type(value) is not Node:
+            raise TypeError("next_node must be a Node object")
+        else:
+            self.__next_node = value
+
+
+class SinglyLinkedList:
+    """ Represent a singly-linked list """
+
+    def __init__(self):
+        """ Initalize a new SinglyLinkedList  """
+        self.__head = None
+
+    def __str__(self):
+        """Define the print representation of a SinglyLinkedList."""
+        if self.__head is None:
+            return ""
+        nodes = []
+        current = self.__head
+        while current is not None:
+            nodes.append(str(current.data))
+            current = current.next_node
+        return "\n".join(nodes)
+
+    def sorted_insert(self, value):
+        """Insert a new Node to the SinglyLinkedList
+
+        The node is inserted into the list at the correct
+        ordered numerical position
+
+        Args:
+            value (Node): The new Node to insert.
+        """
+        new_node = Node(value)
+        if self.__head is None or value < self.__head.data:
+            new_node.next_node = self.__head
+            self.__head = new_node
+        else:
+            current = self.__head
+            while current.next_node is not None
+            and value > current.next_node.data:
+                current = current.next_node
+            new_node.next_node = current.next_node
+            current.next_node = new_node
