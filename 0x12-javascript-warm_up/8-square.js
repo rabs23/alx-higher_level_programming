dc016fa4b2f2d114316b1e14cbd516fde6907bbb
rabs23 <ruthrabs22@gmail.com>
2024-03-11 14:54:31 -0700

    8-square.js

---
+++ 0x12-javascript-warm_up/8-square.js
@@ -0,0 +1,15 @@
+#!/usr/bin/node
+
+if (typeof process.argv[2] === 'undefined' ||
+ isNaN(process.argv[2])) {
+  console.log('Missing size');
+} else {
+  const num = Number(process.argv[2]);
+  for (let i = 0; i < num; i++) {
+    let row = '';
+    for (let j = 0; j < num; j++) {
+      row += 'X';
+    }
+    console.log(row);
+  }
+}
