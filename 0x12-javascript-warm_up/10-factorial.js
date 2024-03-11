5dcdb910e822469abbaf6e594c74ca8893d2bd79
rabs23 <ruthrabs22@gmail.com>
2024-03-11 15:04:42 -0700

    10-factorial.js

---
+++ 0x12-javascript-warm_up/10-factorial.js
@@ -0,0 +1,19 @@
+#!/usr/bin/node
+
+function factorial (n) {
+  if (n === 1) {
+    return 1;
+  } else {
+    return n * factorial(n - 1);
+  }
+}
+
+if (
+  typeof process.argv[2] === 'undefined' ||
+  isNaN(parseInt(process.argv[2], 10)) === true ||
+  parseInt(process.argv[2], 10) < 0
+) {
+  console.log(1);
+} else {
+  console.log(factorial(parseInt(process.argv[2], 10)));
+}
